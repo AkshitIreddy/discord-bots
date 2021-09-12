8898d0533505e4d85e06e3d7788ab2f5e9fc7643
AkshitReddy <90443032+AkshitReddy@users.noreply.github.com>
2021-09-12 06:02:45 +0530

    Add files via upload

---
+++ beginner.py
@@ -0,0 +1,46 @@
+#PROJECT STARTDATE : 9/10/2021
+#PROJECT ENDDATE : 9/11/2021
+#PROJECT DURATION : 15 HOURS
+
+#Few things to note
+#this bot will only read and send messages
+#to avoid spamming the bot will reply to commands that start with '.'
+
+
+import discord
+
+
+#BEFORE RUNNING THIS PROGRAM ADD A TOKEN ABOVE
+#Client intialised
+client = discord.Client()
+
+#this tells us whether our bot has been intialized or not
+@client.event 
+async def on_ready():  
+    print(f'We have logged in as {client.user}')
+
+
+#this records the messages of each user and the messsages that they send
+@client.event
+async def on_message(message):
+    print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+
+
+#The main part of the program
+@client.event
+async def on_message(message):
+  
+    #This will generate a list of things it can do when user says some command word. You can whatever you want for now i will choose 'hi'
+    elif "hi" == message.content.lower():
+      #This will send a message if user says hi. you can change the message to whatever you like just make sure it is under single quotes or double quotes
+        await message.channel.send("HI I,m a discord chatbot")
+
+        
+     #This will send a message if user says bye. you can change the message to whatever you like just make sure it is under single quotes or double quotes
+    elif ".bye" == message.content.lower():
+      # you can say bye by using the name of the person who said bye using this '{message.author.name}' just make sure that you put an 'f' before the single/double quote
+        await message.channel.send(f"Bye see you soon {message.author.name} " 
+
+
+        
+client.run(token)
